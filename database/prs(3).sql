ba373e55297b2ba566fdae2570bdbac80607fa88
codeverse-mw <137955516+eddieprog756@users.noreply.github.com>
2024-12-01 21:45:58 +0200

    With database

--- database/prs(3).sql
+++ database/prs(3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 23, 2024 at 09:53 PM
+-- Generation Time: Dec 01, 2024 at 07:44 PM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -41,21 +41,23 @@ CREATE TABLE IF NOT EXISTS `jobcards` (
   `Job_Description` text NOT NULL,
   `Prepaired_By` varchar(100) NOT NULL,
   `Total_Charged` decimal(10,2) NOT NULL,
+  `payment_proof` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `status` enum('project','sales_done','manager_approved','studio_done','workshop_done','accounts_done') NOT NULL DEFAULT 'project',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `jobcards`
 --
 
-INSERT INTO `jobcards` (`id`, `Date`, `Time`, `JobCard_N0`, `Client_Name`, `Project_Name`, `Quantity`, `Overall_Size`, `Delivery_Date`, `Job_Description`, `Prepaired_By`, `Total_Charged`, `created_at`, `status`) VALUES
-(8, '2024-11-04', '17:36:50', 'JCN09801', 'NBM', 'Posters and Books', 68, '700', '2024-11-25', 'Posters and Books for National Bank.', 'sales User', '320000.00', '2024-11-03 22:00:00', 'accounts_done'),
-(9, '2024-11-04', '17:48:04', 'JCN09802', 'NBS Bank', 'Stationery', 800, '800', '2024-10-15', 'Send Stationery to reserve', 'sales User', '2600000.00', '2024-11-03 22:00:00', 'sales_done'),
-(6, '2024-11-01', '19:19:38', 'JCN09799', 'Deloitte & Touche', 'Banners', 24, '600', '2024-11-28', 'Banners for Projects', 'Admin User', '800000.00', '2024-10-31 22:00:00', 'manager_approved'),
-(7, '2024-11-01', '19:47:07', 'JCN09800', 'TNM', 'TNM Poster', 33, '900', '2024-11-28', 'We need bruh ', 'Admin User', '120000.00', '2024-10-31 22:00:00', 'sales_done'),
-(14, '2024-11-10', '08:39:25', 'JCN09803', 'Liverpool Trust', 'Banners and Rollers', 20, '900', '2024-12-26', 'Printed Banners and Rollers ', 'sales User', '120000.00', '2024-11-09 22:00:00', 'manager_approved');
+INSERT INTO `jobcards` (`id`, `Date`, `Time`, `JobCard_N0`, `Client_Name`, `Project_Name`, `Quantity`, `Overall_Size`, `Delivery_Date`, `Job_Description`, `Prepaired_By`, `Total_Charged`, `payment_proof`, `created_at`, `status`) VALUES
+(8, '2024-11-04', '17:36:50', 'JCN09801', 'NBM', 'Posters and Books', 68, '700', '2024-11-25', 'Posters and Books for National Bank.', 'Sales User', '320000.00', NULL, '2024-11-03 22:00:00', 'accounts_done'),
+(9, '2024-11-04', '17:48:04', 'JCN09802', 'NBS Bank', 'Stationery', 800, '800', '2024-10-15', 'Send Stationery to reserve', 'Sales User', '2600000.00', NULL, '2024-11-03 22:00:00', 'manager_approved'),
+(6, '2024-11-01', '19:19:38', 'JCN09799', 'Deloitte & Touche', 'Banners', 24, '600', '2024-11-28', 'Banners for Projects', 'Admin User', '800000.00', NULL, '2024-10-31 22:00:00', 'manager_approved'),
+(7, '2024-11-01', '19:47:07', 'JCN09800', 'TNM', 'TNM Poster', 33, '900', '2024-11-28', 'We need bruh ', 'Admin User', '120000.00', NULL, '2024-10-31 22:00:00', 'accounts_done'),
+(14, '2024-11-10', '08:39:25', 'JCN09803', 'Liverpool Trust', 'Banners and Rollers', 20, '900', '2024-12-26', 'Printed Banners and Rollers ', 'Sales User', '120000.00', NULL, '2024-11-09 22:00:00', 'manager_approved'),
+(18, '2024-12-01', '14:57:58', 'JCN09805', 'Eicher Industries', 'Handouts', 100, '1212', '2024-12-24', 'For Training new hires', 'Sales User', '1200000.00', './uploads/payment_proofs/WhatsApp Image 2024-06-26 at 01.58.50_422cf255.jpg', '2024-11-30 22:00:00', 'sales_done');
 
 -- --------------------------------------------------------
 
@@ -105,11 +107,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `email`, `full_name`, `created_at`, `profile_pic`, `status`) VALUES
-(1, 'Eddie', '$2y$10$igujLlK2ji5RElX/m25Qzuw4iH3w3w0QXv/AIFiMoGDABInRUSJc6', 'admin', 'temboedward756@gmail.com', 'Admin User', '2024-07-29 23:04:03', './Images/afsff.png', 1),
+(1, 'Manager', '$2y$10$igujLlK2ji5RElX/m25Qzuw4iH3w3w0QXv/AIFiMoGDABInRUSJc6', 'admin', 'temboedward756@gmail.com', 'Manager User', '2024-07-29 23:04:03', './Images/afsff.png', 1),
 (2, 'Designer', '$2y$10$cx6Y5TKXueLA.tKh2MVtwuIOTZbvTEwKshAcfxYx7/5EMU4O3SIf2', 'workshop', 'codeverse.mw@gmail.com', 'Designer User', '2024-07-29 23:04:03', './Images/default_profile.JPG', 0),
-(3, 'Edward', '$2y$10$OXPK8s1zquRE5wQmFSXAguB3Sdppq7Oh1cgeHV7Ngi93IL7UAv53y', 'sales', 'sales@example.com', 'sales User', '2024-07-29 23:04:03', './Images/292047388_411050851040684_2166795279741415865_n.jpg', 0),
-(4, 'George', '$2y$10$JvK252GaytCEkYFo8PnnhOR2O6qDvUep0/2VLzBc2woxbEXRNpGuO', 'workshop', 'temboedward756+12@gmail.com', 'Edward', '2024-09-18 12:56:48', './Images/default_profile.JPG', 1),
-(10, 'Mr G2', '$2y$10$U0DqEu8wLt7aFlmehQOvEOVocIXNlyUftuiLTXbqGU1h/PmQX0sRG', 'designer', 'georgekumwenda0+mrg@gmail.com', 'George Kumwenda', '2024-11-01 17:39:56', './Images/default_profile.JPG', 1);
+(3, 'Adminstator', '$2y$10$OXPK8s1zquRE5wQmFSXAguB3Sdppq7Oh1cgeHV7Ngi93IL7UAv53y', 'sales', 'sales@example.com', 'Sales User', '2024-07-29 23:04:03', './Images/afsff.png', 1),
+(4, 'George', '$2y$10$JvK252GaytCEkYFo8PnnhOR2O6qDvUep0/2VLzBc2woxbEXRNpGuO', 'workshop', 'temboedward756+12@gmail.com', 'Edward', '2024-09-18 12:56:48', './Images/default_profile.JPG', 0),
+(10, 'Mr G2', '$2y$10$U0DqEu8wLt7aFlmehQOvEOVocIXNlyUftuiLTXbqGU1h/PmQX0sRG', 'workshop', 'georgekumwenda0+mrg@gmail.com', 'George Kumwenda', '2024-11-01 17:39:56', './Images/default_profile.JPG', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
